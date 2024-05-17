fa6a2722617543c7664078071adfc9eefd7f721b
dirdr <adrien.pelfresne@gmail.com>
2024-05-17 11:41:18 +0200

    changed ndarray feat flags

--- src/xor.rs
+++ src/xor.rs
@@ -1,3 +1,4 @@
+use mnist::build_network;
 use nn_lib::{
     cost::CostFunction,
     initialization::InitializerType,
@@ -8,9 +9,17 @@ use nn_lib::{
 /// This simple xor neural netowork is used to :
 /// 1. test our neural network with predictable output
 /// 2. improve performances
-struct Xor;
+struct Xor {
+    neural_network: NeuralNetwork,
+}
 
 impl Xor {
+    fn new() -> anyhow::Result<Self> {
+        Ok(Self {
+            neural_network: Self::build_neural_net()?,
+        })
+    }
+
     fn build_neural_net() -> anyhow::Result<NeuralNetwork> {
         Ok(NeuralNetworkBuilder::new()
             .push_layer(DenseLayer::new(2, 1, InitializerType::He))
@@ -20,12 +29,9 @@ impl Xor {
             .with_epochs(100)
             .build()?)
     }
-}
 
-fn xor_test_neural_network() -> anyhow::Result<()> {
-    let mut xor_network_buildr = NeuralNetworkBuilder::new();
-    xor_network_buildr
-        .push_layer(DenseLayer::new(2, 1, InitializerType::He))
-        .buld()?;
-    Ok(())
+    fn start(&mut self) -> anyhow::Result<()> {
+        Array3::self.neural_network.train();
+        Ok(())
+    }
 }
