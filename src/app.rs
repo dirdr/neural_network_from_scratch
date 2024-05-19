00b788322642dba67c51e2e86e44583364d97fcd
AlexVplle <alexisvapaille11@gmail.com>
2024-05-19 15:21:28 +0200

    add gui

---
+++ src/app.rs
@@ -0,0 +1,14 @@
+use egui::{CentralPanel, Context, Pos2};
+
+#[derive(Default)]
+pub struct App {
+    points: Vec<Pos2>
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello World!")
+        });
+    }
+}
